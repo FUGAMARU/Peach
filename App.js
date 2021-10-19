bdb2a061d87fce64f17469ae0ea0c2f6917508f3
FUGAMARU <gofugashi@gmail.com>
2021-10-20 01:24:24 +0900

    [Update] ヘッダーのコンポーネント配置を変更

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, Dimensions} from 'react-native';
 
-import { Header, Icon } from 'react-native-elements';
+import { LinearGradient } from 'expo-linear-gradient';
+import { Header, Icon, Avatar, FAB } from 'react-native-elements';
 import { useFonts } from 'expo-font';
+
+
+//import { FAB } from 'react-native-paper';
 
 //デバイスの横幅、高さ、倍率
 const { width, height, scale } = Dimensions.get('window');
@@ -20,16 +24,23 @@ export default function App() {
   }else{
     return (
       <>
-          <Header backgroundColor="#FFF" placement="center" leftContainerStyle={{flex: 8}} rightContainerStyle={{flex: 4}}
-            leftComponent={<Text style={styles.headerText}>PEACH</Text>}
-            rightComponent={<Text style={styles.cogIcon}><Icon type="ionicon" name="cog-outline" color="#4d4d4d" size={30} /></Text>}
-          />
+        <Header backgroundColor="#FFF" placement="center" leftContainerStyle={{flex: 8}} rightContainerStyle={{flex: 4}}
+          leftComponent={<><Text style={styles.headerText}>PEACH</Text></>}
+          rightComponent={<>
+                            <View style={styles.rightComponentPosition}>
+                              <Avatar rounded source={require("./assets/profileIcon.jpg")} size={40} />                              
+                            </View>
+                          </>}
+        />
 
         <View style={styles.container}>
           <Text>Open up App.js to start working on your app!</Text>
-          <Text>{height}</Text>
+          <Text>Height: {height}</Text>
+          <Text>Width: {width}</Text>
           <StatusBar style="auto" />
         </View>
+        {//<Text ><Icon type="ionicon" name="cog-outline" color="#4d4d4d" size={34} /></Text>
+        }
       </>
     );
   }
@@ -43,13 +54,16 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   headerText: {
-    color: "#ff4a92",
+    color: "#ff8ab5",
     fontSize: height * 0.045,
     fontFamily: "Arista-Pro",
     marginTop: height * 0.015,
     marginLeft: height * 0.015
   },
-  cogIcon: {
-    marginTop: height * 0.019
-  }
+  rightComponentPosition: {
+    flexDirection: "row",
+    marginTop: height * 0.01,
+    marginBottom: height * 0.1499 * -1,
+    marginRight: width * 0.0186
+  },
 });
