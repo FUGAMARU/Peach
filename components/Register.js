03e524fe027294f07d09d6346d189f4d545802c8
FUGAMARU <gofugashi@gmail.com>
2021-10-31 02:09:53 +0900

    [Add] linkWithCredential(電話番号に仮メアド・パスワード紐付け)を実装

--- components/Register.js
+++ components/Register.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { Button, Text, SafeAreaView,TextInput, View } from "react-native";
 
 import * as firebase from "firebase";
@@ -15,8 +15,9 @@ try{
 const Register = () => {
 	const recaptchaVerifier = useRef(null);
 	const [phoneNumber, setPhoneNumber] = useState();
-	const [verificationId, setVerificationId] = useState();
+	const [verificationId, setVerificationId] = useState(null);
 	const [verificationCode, setVerificationCode] = useState();
+	const [password, setPassword] = useState();
 	const firebaseConfig = firebase.apps.length ? firebase.app().options : undefined;
 
 	return(
@@ -31,12 +32,11 @@ const Register = () => {
 				<Button title="送信" onPress={async () => {
 					try {
 						const phoneProvider = new firebase.auth.PhoneAuthProvider();
-						const verificationId = await phoneProvider.verifyPhoneNumber(
-						phoneNumber,
-						recaptchaVerifier.current
-						);
+						const verificationId = await phoneProvider.verifyPhoneNumber(phoneNumber, recaptchaVerifier.current);
+						console.log("======================verificationID===================");
+						console.log(verificationId);
 						setVerificationId(verificationId);
-						alert("👻認証コードを送信しました");
+						alert("👻認証コードを送信しました");	
 					} catch (err) {
 						alert(`😥エラーが発生しました: ${err.message}`);
 					}
@@ -45,15 +45,30 @@ const Register = () => {
 				<TextInput style={{borderWidth: 1, width: 200, height: 30}} placeholder="123456" keyboardType="number-pad" onChangeText={setVerificationCode} />
 				<Button title="認証" onPress={async () => {
 					try {
-						const credential = firebase.auth.PhoneAuthProvider.credential(
-						verificationId,
-						verificationCode
-						);
-						await firebase.auth.signInWithCredential(credential);
+						const credential = firebase.auth.PhoneAuthProvider.credential(verificationId, verificationCode);
+						await firebase.auth().signInWithCredential(credential);
 						alert("✅認証が完了しました！");
+						console.log(firebase.auth().currentUser);
 					} catch (err) {
 						alert(`😥エラーが発生しました: ${err.message}`);
 				}}} />
+				<Text style={{marginTop: 20}}>パスワード</Text>
+				<TextInput style={{borderWidth: 1, width: 200, height: 30}} placeholder="Type password here" keyboardType="ascii-capable" onChangeText={setPassword} />
+				<Button title="確定" onPress={async () => {
+					const credential = firebase.auth.EmailAuthProvider.credential(phoneNumber + "@peach.firebase.google.com", password);
+					console.log("==================Credential====================");
+					console.log(credential);
+					console.log("==================CurrentUser====================");
+					console.log(firebase.auth().currentUser);
+					firebase.auth().currentUser.linkWithCredential(credential)
+					.then((usercred) => {
+					  const user = usercred.user;
+					  console.log("Account linking success", user);
+					  alert("Successfully registering your password !");
+					}).catch((error) => {
+					  alert("Account linking error", error);
+					});				  
+				}} />
 			</View>
 		</>
 	);
